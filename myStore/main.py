c7faa0e806709e21869760302c2c6c77eb51836c
DiamondAngel1 <tararukolga78@gmail.com>
2026-02-18 14:44:43 +0200

    postgres run

--- myStore/main.py
+++ myStore/main.py
@@ -1,29 +1,29 @@
+#!/usr/bin/env python3
 import os
+import subprocess
 import boto3
+from datetime import datetime
 
-#print("Сало - це смачно і дуже корисно!")
-s3_client = boto3.client('s3')
+# Налаштування
+DB_CONTAINER = "postgres"   # ім'я контейнера
+DB_NAME = "mydb"
+DB_USER = "postgres"
+S3_BUCKET = "my-backup-bucket"
+BACKUP_DIR = "/tmp"
 
-response = s3_client.list_buckets()
-for bucket in response['Buckets']:
-    print(bucket)
+# Формуємо ім'я файлу
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+backup_file = f"{BACKUP_DIR}/{DB_NAME}_{timestamp}.sql"
 
-response = s3_client.list_objects_v2(Bucket='transfer-siu-images')
-objects = response.get('Contents', [])
-print(objects)
+# Виконуємо pg_dump всередині контейнера
+with open(backup_file, "w") as f:
+    subprocess.run([
+        "docker", "exec", DB_CONTAINER,
+        "pg_dump", "-U", DB_USER, DB_NAME
+    ], stdout=f, check=True)
 
-#s3_client.download_file('transfer-siu-images', 'bobr.jpg', 'downloaded_bobr.jpg')
-#s3_client.download_file('transfer-siu-images', 'info.txt', 'downloaded_info.txt')
+# Завантажуємо на S3
+s3 = boto3.client("s3")
+s3.upload_file(backup_file, S3_BUCKET, os.path.basename(backup_file))
 
-s3_client.put_bucket_versioning(
-    Bucket='transfer-siu-images', 
-    VersioningConfiguration={'Status': 'Enabled'}
-)
-
-response = s3_client.list_object_versions(
-    Bucket='transfer-siu-images',
-    Prefix='info.txt'
-)
-
-for version in response.get('Versions'):
-    print(version)
+print(f"Backup {backup_file} uploaded to s3://{S3_BUCKET}/")
